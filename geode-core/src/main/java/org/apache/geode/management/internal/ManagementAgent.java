34cc737406a7f3df78db7a4759f0786bf1bbddd6
Kirk Lund <klund@apache.org>
2022-02-23 16:10:01 -0800

    Revert "GEODE-9980: Improve error handling of serial filters (#7299)"
    This reverts commit ce57e9fd2b8b644cadc469209e12e4fbd281e0d9.

--- geode-core/src/main/java/org/apache/geode/management/internal/ManagementAgent.java
+++ geode-core/src/main/java/org/apache/geode/management/internal/ManagementAgent.java
@@ -69,7 +69,6 @@ import org.apache.geode.internal.security.SecurableCommunicationChannel;
 import org.apache.geode.internal.security.SecurityService;
 import org.apache.geode.internal.security.shiro.JMXShiroAuthenticator;
 import org.apache.geode.internal.serialization.filter.FilterConfiguration;
-import org.apache.geode.internal.serialization.filter.UnableToSetSerialFilterException;
 import org.apache.geode.internal.tcp.TCPConduit;
 import org.apache.geode.logging.internal.log4j.api.LogService;
 import org.apache.geode.management.ManagementException;
@@ -140,11 +139,7 @@ public class ManagementAgent {
   }
 
   public synchronized void startAgent() {
-    try {
-      filterConfiguration.configure();
-    } catch (UnableToSetSerialFilterException e) {
-      throw new RuntimeException(e);
-    }
+    filterConfiguration.configure();
     loadWebApplications();
 
     if (!running && config.getJmxManagerPort() != 0) {
